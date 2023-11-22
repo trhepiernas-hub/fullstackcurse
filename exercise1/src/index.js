fb81e94c9e468c1d437003b86adc41a0f1aec9f3
Trhe_piernas <hruizdeegino@gmail.com>
2023-11-22 22:25:01 +0100

    ariketa 61-6 hasi

--- exercise1/src/index.js
+++ exercise1/src/index.js
@@ -1,56 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      {props.part} {props.exercises}
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <p><Part part={props.parts[0].name} exercises={props.parts[0].exercises} /></p>
-      <p><Part part={props.parts[1].name} exercises={props.parts[1].exercises} /></p>
-      <p><Part part={props.parts[2].name} exercises={props.parts[2].exercises} /></p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return <p>Número de ejercicios {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>give feedbak</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
